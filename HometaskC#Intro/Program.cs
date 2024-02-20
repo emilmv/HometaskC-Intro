f79dbdb5194f21c8e6de51f1bbc487622b836c74
emilmv <emilnm@code.edu.az>
2024-02-20 10:40:10 +0400

    Task4V2 added

--- HometaskC#Intro/Program.cs
+++ HometaskC#Intro/Program.cs
@@ -2,50 +2,94 @@
 Console.WriteLine("Hello, World!");
 #region Task4
 
-int[] arr = { 111, 222, 1111, 2222, 11111, 22222, 111111 };
+//int[] arr = { 111, 222, 1111, 2222, 11111, 22222, 111111 };
 
-int sumOfThreeDigits = 0;
-int sumOfFourDigits = 0;
-int sumOfFiveDigits = 0;
-int sumOfSixDigits = 0;
-int multOfThreeDigits = 1;
-int multOfFourDigits = 1;
-double result = 0;
+//int sumOfThreeDigits = 0;
+//int sumOfFourDigits = 0;
+//int sumOfFiveDigits = 0;
+//int sumOfSixDigits = 0;
+//int multOfThreeDigits = 1;
+//int multOfFourDigits = 1;
+//double result = 0;
 
-for (int i = 0; i < arr.Length; i++)
-{
-    if (arr[i] > 99 && arr[i] < 1000)
-    {
-        sumOfThreeDigits += arr[i];
-        multOfThreeDigits *= arr[i];
-    }
-    if (arr[i] > 999 && arr[i] < 10000)
-    {
-        sumOfFourDigits += arr[i];
-        multOfFourDigits*= arr[i];
-    }
-    if (arr[i] > 9999 && arr[i] < 100000)
-    {
-        sumOfFiveDigits += arr[i];
-    }
-    if (arr[i] > 99999 && arr[i] < 1000000)
-    {
-        sumOfSixDigits += arr[i];
-    }
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] > 99 && arr[i] < 1000)
+//    {
+//        sumOfThreeDigits += arr[i];
+//        multOfThreeDigits *= arr[i];
+//    }
+//    if (arr[i] > 999 && arr[i] < 10000)
+//    {
+//        sumOfFourDigits += arr[i];
+//        multOfFourDigits *= arr[i];
+//    }
+//    if (arr[i] > 9999 && arr[i] < 100000)
+//        sumOfFiveDigits += arr[i];
 
-}
-result = sumOfThreeDigits + multOfFourDigits;
-result *= 10;
-result += 7;
-result += sumOfFiveDigits;
-result -= (multOfThreeDigits * 10 + 1);
-result += sumOfSixDigits;
-result -= (sumOfThreeDigits + sumOfFourDigits);
-result *= 0.18;
-result *= 0.03;
-result *= 0.01;
-result += sumOfFiveDigits;
+//    if (arr[i] > 99999 && arr[i] < 1000000)
+//        sumOfSixDigits += arr[i];
+//}
+//result = sumOfThreeDigits + multOfFourDigits;
+//result *= 10;
+//result += 7;
+//result += sumOfFiveDigits;
+//result -= (multOfThreeDigits * 10 + 1);
+//result += sumOfSixDigits;
+//result -= (sumOfThreeDigits + sumOfFourDigits);
+//result *= 0.18;
+//result *= 0.03;
+//result *= 0.01;
+//result += sumOfFiveDigits;
 
-Console.WriteLine(result);
+//Console.WriteLine(result);
+
+// result = 34660.542156
+
+#endregion
+
+#region Task4V2
+
+//int[] arr = { 111, 222, 1111, 2222, 11111, 22222, 111111 };
+
+//double result = 0;
+//int sumOfThreedigits = 0;
+//int sumOfFourDigits = 0;
+//int sumOfFiveDigits = 0;
+//int sumOfSixDigits = 0;
+//int multOfThreeDigits = 1;
+//int multOfFourDigits = 1;
+//int count = 0;
+
+//foreach (var i in arr)
+//{
+//    if (i / 10 < 100)
+//    {
+//        sumOfThreedigits += i;
+//        multOfThreeDigits *= i;
+//    }
+//    if (i / 10 < 1000 && i / 10 > 99)
+//    {
+//        sumOfFourDigits += i;
+//        multOfFourDigits *= i;
+//    }
+//    if (i / 10 < 10000 && i / 10 > 999)
+//        sumOfFiveDigits += i;
+//    if (i / 10 < 100000 && i / 10 > 9999)
+//        sumOfSixDigits += i;
+//}
+//result = sumOfThreedigits + multOfFourDigits;
+//result *= 10;
+//result += 7;
+//result += sumOfFiveDigits;
+//result -= (multOfThreeDigits * 10 + 1);
+//result += sumOfSixDigits;
+//result -= (sumOfThreedigits + sumOfFourDigits);
+//result *= 0.18;
+//result *= 0.03;
+//result *= 0.01;
+//result += sumOfFiveDigits;
+
+//Console.WriteLine(result);
 
 #endregion
